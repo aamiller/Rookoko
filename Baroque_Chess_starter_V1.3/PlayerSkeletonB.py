1e77b4f4cdf107a1cff9d4edd94d28a8ce482505
Cillian Murphy <cillian@murphnduff.org>
2018-03-07 14:32:33 -0800

    still a silly potato

--- Baroque_Chess_starter_V1.3/PlayerSkeletonB.py
+++ Baroque_Chess_starter_V1.3/PlayerSkeletonB.py
@@ -22,7 +22,7 @@ def makeMove(currentState, currentRemark, timelimit):
 
     # Fix up whose turn it will be.
     newState.whose_move = 1 - currentState.whose_move
-    possible_boards = generate_successors()
+    possible_boards = generate_successors(currentState.board, currentState.whose_move)
     new_move, new_board = random.choice(possible_boards)
     newState.board = new_board
 
